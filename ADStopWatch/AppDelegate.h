542c80019e4d67da7182aa45be9175786e6bc9b0
Student P_03 <studentp_03@student-p-03.local>
2016-08-02 11:44:46 +0530

    Initial Commit

---
+++ ADStopWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADStopWatch
+//
+//  Created by Student P_03 on 02/08/16.
+//  Copyright © 2016 Amol Dhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADStopWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADStopWatch
+//
+//  Created by Student P_03 on 02/08/16.
+//  Copyright © 2016 Amol Dhage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
